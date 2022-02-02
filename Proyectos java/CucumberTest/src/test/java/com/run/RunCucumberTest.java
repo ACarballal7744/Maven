220c1921811fbc20ff778414bd6cc5b6815a898f
ACarballal7744 <alan.carba@hotmail.com>
2022-02-02 16:53:55 -0300

    2do commit

--- Proyectos java/CucumberTest/src/test/java/com/run/RunCucumberTest.java
+++ Proyectos java/CucumberTest/src/test/java/com/run/RunCucumberTest.java
@@ -9,7 +9,8 @@ import io.cucumber.junit.CucumberOptions;
 //@Ignore("Comentar o quitar esta linea para ejecutar los contratos Gherkins.")
 @RunWith(Cucumber.class)
 //@CucumberOptions(plugin = { "pretty" })
-@CucumberOptions(features = "src/test/resources/com/service/test/search.feature",glue = "src/test/java",plugin = { "pretty" }, tags = "not @ignore")
+@CucumberOptions(features = "src/test/resources/com/service/test/search.feature",
+glue = {"steps"},plugin = { "pretty" }, tags = "not @ignore")
 //@CucumberOptions(plugin = {"pretty", "html:target/cucumber"})
 public class RunCucumberTest {
 }
